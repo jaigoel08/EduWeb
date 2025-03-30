84a05442dcc219c282b680fbdba9e6a96be06be1
Jai Goyal <jaigoel14@gmail.com>
2025-03-30 15:16:06 +0530

    new commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://eduweb-backend-gphf.onrender.com', // Your backend URL
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
+}) 
